1d4c5c8775bd3b5a870af9ba209112681f1ab253
CK-261-web <pchethankarthik@gmail.com>
2025-02-12 21:02:24 -0600

    activity1done

---
+++ lesson-1/Activity1.java
@@ -0,0 +1,13 @@
+package lesson1;
+import java.util.Scanner;
+
+public class Activity1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number * number);
+        scanner.close();
+    }
+
+}
